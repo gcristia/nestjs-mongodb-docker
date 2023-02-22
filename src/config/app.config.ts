0a4af637fa3bcbc58202d5217e14d004a09c45fe
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2023-02-22 13:20:12 -0500

    Update Environments

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,6 @@
 export const EnvConfigurations = () => ({
     environment: process.env.NODE_ENV || 'development',
-    mongoDb: process.env.MONGO_DB,
+    mongoDbUri: process.env.MONGODB_URI,
     port: process.env.PORT || 3000,
     defaultLimit: +process.env.DEFAULT_LIMIT || 10,
 })
--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -1,7 +1,7 @@
 import * as Joi from 'joi'
 
 export const JoiValidationSchema = Joi.object({
-    MONGO_DB: Joi.required(),
+    MONGODB_URI: Joi.required(),
     PORT: Joi.number().default(3000),
     DEFAULT_LIMIT: Joi.number().default(10),
     DEFAULT_OFFSET: Joi.number().default(0),
